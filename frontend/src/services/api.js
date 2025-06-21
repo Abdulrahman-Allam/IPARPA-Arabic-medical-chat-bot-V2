c308077d948430d30473d79a9832121fd1ce0605
Abdulrahman Allam <abdulrahman.allam.cs@gmail.com>
2025-06-21 04:07:09 +0300

    Added Vercel API URL in Frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_VERCEL_API_URL;
 
 // Create axios instance with error handling
 const api = axios.create({
